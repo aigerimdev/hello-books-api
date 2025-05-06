3712f014d4956be0e72c2b9041cd8513e93d7a7c
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-05-05 23:21:17 -0700

    adds new model one to many

--- app/models/book.py
+++ app/models/book.py
@@ -1,22 +1,35 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from typing import Optional
 from ..db import db
 
-class Book(db.Model): # gonna take this Book model it is gonna be a books table in  my database
+class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
+    author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("author.id"))
+    author: Mapped[Optional["Author"]] = relationship(back_populates="books")
+
+    def to_dict(self):
+        book_as_dict = {}
+        book_as_dict["id"] = self.id
+        book_as_dict["title"] = self.title
+        book_as_dict["description"] = self.description
+
+        if self.author:
+            book_as_dict["author"] = self.author.name
+
+        return book_as_dict
     
     @classmethod
-    def from_dict(cls, data):
-        return cls(
-            title=data["title"],
-            description=data["description"]
+    def from_dict(cls, book_data):
+        # Use get() to fetch values that could be undefined to avoid raising an error
+        author_id = book_data.get("author_id")
+
+        new_book = cls(
+            title=book_data["title"],
+            description=book_data["description"],
+            author_id=author_id
         )
-    # my_book = Book.from_dict(book_data)
-    
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "description":self.description
-        }
+
+        return new_book
